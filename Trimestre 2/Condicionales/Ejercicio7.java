5ebb089b722fcbf405e73b2171ce9c27f6453721
EstebanKrD <krdonaj@gmail.com>
2025-10-10 15:05:28 -0500

    Se agrega nuevos ejercicios

---
+++ Trimestre 2/Condicionales/Ejercicio7.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese el operador 1: ");
+        int operador1 = scanner.nextInt();
+        System.out.println("ingrese el operador 2: ");
+        int operador2 = scanner.nextInt();
+        scanner.nextLine();
+
+        
+    }
+}
