90be37368673e02d8abf3820b53f0070d6bcd3fc
EstebanKrD <krdonaj@gmail.com>
2025-10-14 14:41:12 -0500

    Se realzan 3 ejercicios

---
+++ Trimestre 2/Ciclos/Ejercicio6.java
@@ -0,0 +1,11 @@
+package Ciclos;
+
+import java.util.Scanner;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Indique la base : ");
+        int operador1 = scanner.nextInt();
+    }
+}
